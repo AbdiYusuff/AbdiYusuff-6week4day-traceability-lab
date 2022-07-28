ed549316177aed998cc3c59df0d4666054869ddb
Abdi Yusuf <ayusufadan@gmail.com>
2022-07-28 15:41:48 -0500

    rollbar imported

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '1f7981afe6c546da90b12a5de721a6c8',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 
 app.use(express.json())
